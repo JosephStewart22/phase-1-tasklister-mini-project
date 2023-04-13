5af848fa8ca57f2b6a6434c613e8cbab162a8d2b
Joseph Stewart <stewartjoseph255@gmail.com>
2023-04-13 09:59:28 -0600

    Mini-Project Listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    addToDo(e.target.newtaskdescription.value);
+  })
 });
+
+//input.addEventListener('submit', addToDo);
+
+function addToDo(task) {
+  const p = document.createElement('p');
+  p.textContent = task;
+  document.querySelector('#list').appendChild(p);
+}
+
+
